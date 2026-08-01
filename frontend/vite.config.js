d5bfde9eb0cb2c5d2b2680fce5c9f33f0ab946bc
Tasin Tausif <tasintausif@gmail.com>
2026-08-01 12:51:11 +0600

    Deployment changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:8000',
+                target: env.VITE_BACKEND_URL,
                 changeOrigin: true
             }
         }
